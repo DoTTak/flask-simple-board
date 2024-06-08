474dbef0171f0bb72d112e40e4f4bcf302284abe
DoTTak <kdw8220@gmail.com>
2024-06-09 00:24:13 +0900

    backend: 회원가입 - 회원가입 기능 구현 완료

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from flask import Flask, redirect, request, render_template, session
 from flask_mail import Mail, Message
 from apps.posts import posts_app
 from apps.users import users_app
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.secret_key = config.SECRET_KEY
@@ -21,6 +22,10 @@ app.config['MAIL_USE_TLS'] = config.MAIL_USE_TLS
 app.config['MAIL_USE_SSL'] = config.MAIL_USE_SSL
 mail = Mail(app)
 
+app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['BCRYPT_LEVEL'] = 10
+bcrypt = Bcrypt(app)
+
 @app.route('/')
 def index():
     return redirect('/posts')
@@ -33,13 +38,92 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    input_data = {"input_data": {"email": "", "email_auth": "", "token": "", "name": "", "school": ""}}
     if request.method == 'GET':
         # 세션 초기화
         session.clear()
-        return render_template("pages/register.html")
+        response = {"data": input_data}
+        return render_template("pages/register.html", response=response)
     else:
-        print(session)
-        return "Hello"
+        email = request.form.get("form-email", "").strip()
+        email_auth = request.form.get("form-email-auth", "").strip()
+        token = request.form.get("form-email-token", "").strip()
+        password = request.form.get("form-password", "").strip()
+        password2 = request.form.get("form-password2", "").strip()
+        name = request.form.get("form-name", "").strip()
+        school = request.form.get("form-school", "").strip()
+
+        # 이메일 인증번호가 다를 경우 인증번호 초기화
+        if email_auth != session.get(f"otp_{email}", None):
+            session.clear()
+            return render_template("pages/register.html", response={"status": "error", "msg": "인증번호가 다릅니다.\\n다시 시도해주세요.", "redirect_url": "/register", "data": input_data})
+
+        # 클라이언트에 새로고침 발생 시 데이터를 다시 입력하기 위함
+        input_data['input_data'] = {
+            "email": email,
+            "email_auth": email_auth,
+            "token": token,
+            # "password": password, # 비밀번호는 제외
+            # "password2": password2, # 비밀번호는 제외
+            "name": name,
+            "school": school
+        }
+        if not email or not email_auth or not token or not password or not password2 or not name or not school:
+            return render_template("pages/register.html", response={"status": "error", "msg": "입력값을 확인해주세요.", "data": input_data})
+
+        # 패스워드 확인        
+        if password != password2:
+            return render_template("pages/register.html", response={"status": "error", "msg": "비밀번호가 같지 않습니다.\\n다시 확인해주세요.", "data": input_data})
+        
+        # 길이 검사
+        if len(password) < 8:
+            return render_template("pages/register.html", response={"status": "error", "msg": "패스워드가 너무 짧습니다.(8자 이상))", "data": input_data})
+        if len(name) < 4:
+            return render_template("pages/register.html", response={"status": "error", "msg": "이름이 너무 짧습니다.(4자 이상))", "data": input_data})
+
+        
+        # 이메일 중복검사, 이메일 인증 요청, 이메일 인증 번호 입력 모두 완료해야
+        # 회원가입 실시
+        if session.get("is_email_check", None) and session.get("is_send_email", None) and session.get("is_email_auth", None):
+            # 이메일 세션 검증 완료 했으니 초기화
+            session.clear()
+
+            conn = pymysql.connect(host=config.DB_HOST, user=config.DB_USER, password =config.DB_PASSWORD, db=config.DB_DATABSE, charset='utf8')
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            
+            # 이메일이 존재하는지 확인하기 위한 SQL 구문
+            sql = f"""
+            SELECT `email` FROM `users` WHERE email=%s
+            """
+            cursor.execute(sql, (email))
+            is_exists_email = cursor.fetchone()
+            
+            # 회원가입 insert전, 한번 더 이메일 중복 여부 검사
+            if is_exists_email:
+                cursor.close()
+                conn.close()
+
+                status = "error"
+                msg = "이미 사용중인 이메일 주소입니다.\\n다시 시도해주세요."
+                return render_template("pages/register.html", response={"status": "error", "msg": "비밀번호가 같지 않습니다.\\n다시 확인해주세요.", "data": input_data})
+                
+            else:
+                sql = f"""
+                INSERT INTO
+                    `users` (`email`, `name`, `password`, `school`)
+                VALUES
+                    (%s, %s, %s, %s);
+                """
+                cursor.execute(sql, (email, name, bcrypt.generate_password_hash(password), school))
+                conn.commit()
+                return render_template("pages/register.html", response={"status": "success", "msg": f"{name}님, 회원가입을 축하드립니다", "redirect_url": "/login", "data": input_data})
+
+            cursor.close()
+            conn.close()
+
+        else:
+            return render_template("pages/register.html", response={"status": "error", "msg": "이메일 인증 과정이 비정상적입니다.\\n다시 시도해주세요.", "redirect_url": "/register", "data": input_data})
+
 
 
 @app.route('/email_check', methods=['POST'])
